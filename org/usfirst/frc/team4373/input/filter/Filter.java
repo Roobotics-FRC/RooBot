678e968b3a2914d07e9e132882bd6fac8640605e
Henry Pitcairn <735tesla@gmail.com>
2016-01-29 16:20:39 -0500

    fewoiwjfoiewjo YAY broseph

--- org/usfirst/frc/team4373/input/filter/Filter.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team4373.input.filter;
-
-/**
- * Created by tesla on 11/1/15.
- */
-public interface Filter {
-    public Object applyFilter(Object input);
-}
--- org/usfirst/frc/team4373/robot/RobotMap.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team4373.robot;
-
-/**
- * Created by tesla on 11/1/15.
- */
-public class RobotMap {
-    public static final int JOYSTICK_PORT = 1;
-}
