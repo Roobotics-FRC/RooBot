2b8a4f02278f8a40e4f7e852628aca8f9c8a0e9d
Henry Pitcairn <735tesla@gmail.com>
2016-02-08 12:00:13 -0500

    added riemann integrator

---
+++ src/org/usfirst/frc/team4373/robot/math/Point.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team4373.robot.math;
+
+/**
+ * Created by tesla on 2/8/16.
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+}
